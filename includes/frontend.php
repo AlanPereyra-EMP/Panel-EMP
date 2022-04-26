32402387795cf05c306694e59ba03340d3fd26ee
Alan Pereyra <alanpereyra33@gmail.com>
2022-04-26 17:22:30 -0300

    Fix language bug and add new searchform

--- includes/frontend.php
+++ includes/frontend.php
@@ -1,20 +1,32 @@
 <?php
 // Change frontend shop title
 function pemp_woocommerce_page_title( $page_title ) {
-  if( $page_title == 'Shop' ) {
+  if( $page_title == 'Tienda' ) {
     return "Propiedades";
   }
 }
 add_filter( 'woocommerce_page_title', 'pemp_woocommerce_page_title');
 
-// Change WooCommerce search product placeholder
-function pemp_change_woocomerce_searchform( $translated_text, $text, $domain ) {
-   switch ( $text ) {
-       case 'Search products&hellip;' :
-           $text = __( 'Buscar Propiedades&hellip;', 'woocommerce' );
-           break;
-   }
-   return $text;
+/**
+ * woo_custom_product_searchform
+ *
+ * @access      public
+ * @since       1.0
+ * @return      void
+*/
+function woo_custom_product_searchform( $form ) {
+
+	$form = '<form role="search" method="get" id="searchform" action="' . esc_url( home_url( '/'  ) ) . '">
+		<div>
+			<label class="screen-reader-text" for="s">' . __( 'Buscado con:', 'woocommerce' ) . '</label>
+			<input type="text" value="' . get_search_query() . '" name="s" id="s" placeholder="' . __( 'Buscar propiedades', 'woocommerce' ) . '" />
+			<input type="submit" id="searchsubmit" value="'. esc_attr__( 'Buscar', 'woocommerce' ) .'" />
+			<input type="hidden" name="post_type" value="product" />
+		</div>
+	</form>';
+
+	return $form;
+
 }
-add_filter( 'gettext', 'pemp_change_woocomerce_searchform', 20, 3 );
+add_filter( 'get_product_search_form' , 'woo_custom_product_searchform' );
 ?>
