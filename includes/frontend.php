f885770be718ebe8f3d2dbd5166c1cd3398c3f87
Alan Pereyra <alanpereyra33@gmail.com>
2022-04-26 17:35:22 -0300

    improve custom search form

--- includes/frontend.php
+++ includes/frontend.php
@@ -7,26 +7,19 @@ function pemp_woocommerce_page_title( $page_title ) {
 }
 add_filter( 'woocommerce_page_title', 'pemp_woocommerce_page_title');
 
-/**
- * woo_custom_product_searchform
- *
- * @access      public
- * @since       1.0
- * @return      void
-*/
-function woo_custom_product_searchform( $form ) {
+function emp_woo_custom_product_searchform( $form ) {
 
 	$form = '<form role="search" method="get" id="searchform" action="' . esc_url( home_url( '/'  ) ) . '">
-		<div>
-			<label class="screen-reader-text" for="s">' . __( 'Buscado con:', 'woocommerce' ) . '</label>
-			<input type="text" value="' . get_search_query() . '" name="s" id="s" placeholder="' . __( 'Buscar propiedades', 'woocommerce' ) . '" />
-			<input type="submit" id="searchsubmit" value="'. esc_attr__( 'Buscar', 'woocommerce' ) .'" />
-			<input type="hidden" name="post_type" value="product" />
-		</div>
-	</form>';
+        		<div>
+        			<label class="screen-reader-text" for="s">' . __( 'Buscado con:', 'woocommerce' ) . '</label>
+        			<input type="text" value="' . get_search_query() . '" name="s" id="s" placeholder="' . __( 'Buscar propiedades', 'woocommerce' ) . '" />
+        			<input type="submit" id="searchsubmit" value="'. esc_attr__( 'Buscar', 'woocommerce' ) .'" />
+        			<input type="hidden" name="post_type" value="product" />
+        		</div>
+        	</form>';
 
 	return $form;
 
 }
-add_filter( 'get_product_search_form' , 'woo_custom_product_searchform' );
+add_filter( 'get_product_search_form' , 'emp_woo_custom_product_searchform' );
 ?>
