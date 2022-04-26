671df559e3db2bcb9f4c0e940e8244c6f99490a5
Alan Pereyra <alanpereyra33@gmail.com>
2022-04-26 18:42:15 -0300

    fix woocommerce's searchform

--- includes/frontend.php
+++ includes/frontend.php
@@ -9,11 +9,11 @@ add_filter( 'woocommerce_page_title', 'pemp_woocommerce_page_title');
 
 function emp_woo_custom_product_searchform( $form ) {
 
-	$form = '<form role="search" method="get" id="searchform" action="' . esc_url( home_url( '/'  ) ) . '">
+	$form = '<form class="woo-searchform" role="search" method="get" id="searchform" action="' . esc_url( home_url( '/'  ) ) . '">
         		<div>
         			<label class="screen-reader-text" for="s">' . __( 'Buscado con:', 'woocommerce' ) . '</label>
         			<input type="text" value="' . get_search_query() . '" name="s" id="s" placeholder="' . __( 'Buscar propiedades', 'woocommerce' ) . '" />
-        			<input type="submit" id="searchsubmit" value="'. esc_attr__( 'Buscar', 'woocommerce' ) .'" />
+        			<input class="btn" type="submit" id="searchsubmit" value="'. esc_attr__( 'Buscar', 'woocommerce' ) .'" />
         			<input type="hidden" name="post_type" value="product" />
         		</div>
         	</form>';
